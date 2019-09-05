7879d1a6693dbd78ee553c6eb4cded1f68da6643
Matheus Oliveira <matheus.vetor@gmail.com>
2019-09-05 20:04:03 -0300

    added new routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      resources :employees do
+        resources :time_entries
+      end
+    end
+  end
 end
