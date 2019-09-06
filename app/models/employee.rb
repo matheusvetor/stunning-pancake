489d0d6738275c70cc5c5bd65380f0c7307822af
Matheus Oliveira <matheus.vetor@gmail.com>
2019-09-06 00:50:26 -0300

    Added missing validation

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -2,6 +2,7 @@ class Employee < ApplicationRecord
   has_secure_password
   has_many :time_entries
   validates :name, :cpf, presence: true
+  validates :cpf, :email, :pis, uniqueness: true
 
   scope :not_admin, -> { where(is_admin: false) }
 end
